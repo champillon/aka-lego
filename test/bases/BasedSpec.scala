be0bb99e72aa39d84bc23b348ba4f1c31a4d869c
passapong.t <passapong.t@gmail.com>
2017-08-14 14:05:47 +0700

    created

---
+++ test/bases/BasedSpec.scala
@@ -0,0 +1,12 @@
+package bases
+
+import org.scalatest.BeforeAndAfterEach
+import org.scalatestplus.play.PlaySpec
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+
+class BasedSpec
+  extends PlaySpec
+    with GuiceOneAppPerSuite
+    with BeforeAndAfterEach {
+
+}
