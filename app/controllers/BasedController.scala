dccfad9f880eeae26258e2e024c71d4270aed77a
passapong.t <passapong.t@gmail.com>
2017-08-16 19:53:55 +0700

    added form, as, NO_BODY, NOT_JSON, INVALID_JSON

--- app/controllers/BasedController.scala
+++ app/controllers/BasedController.scala
@@ -1,17 +1,36 @@
 package controllers
 
-import play.api.libs.json.JsValue
-import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import utilities.Json
 
 abstract class BasedController(cc: ControllerComponents)
   extends AbstractController(cc) {
 
-  protected def success(json: JsValue) = Ok(json.toString) as APPLICATION_JSON
+  protected def form[Form: Manifest](request: Request[AnyContent])
+                                    (action: => Json) = {
+    request match {
+      case _ if !request.hasBody => invalidInput(NO_BODY)
+      case _ if request.body.asJson.isEmpty => invalidInput(NOT_JSON)
+      case _ if as[Form](request).isEmpty => invalidInput(INVALID_JSON)
+      case _ => success(action)
+    }
+  }
 
-  protected def fail(error: String) = InternalServerError(error)
+  protected def as[Object: Manifest](request: Request[AnyContent]): Option[Object] =
+    Json.toObject[Object](request.body.asJson.get.toString)
 
-  protected def invalidInput = BadRequest
+  private def success(json: Json) = Ok(json.toText) as APPLICATION_JSON
+
+  private def fail(error: String) = InternalServerError(error)
+
+  private def invalidInput(error: String) = BadRequest(error)
 
   private lazy val APPLICATION_JSON = "application/json"
+
+  private lazy val NO_BODY = "request have no body."
+
+  private lazy val NOT_JSON = "body is not json format."
+
+  private lazy val INVALID_JSON = "json is not valid."
 
 }
