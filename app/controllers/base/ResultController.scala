016ae7e849965a36999f96fe7565e38f43d01a45
passapong.t <passapong.t@gmail.com>
2017-08-16 23:04:23 +0700

    moved success, success(json), fail, invalidInput from BasedController

---
+++ app/controllers/base/ResultController.scala
@@ -0,0 +1,18 @@
+package controllers.base
+
+import play.api.mvc.{AbstractController, ControllerComponents}
+import utilities.Json
+
+abstract class ResultController(cc: ControllerComponents)
+  extends AbstractController(cc) {
+
+  def success = Ok as APPLICATION_JSON
+
+  def success(json: Json) = Ok(json.toText) as APPLICATION_JSON
+
+  def fail(error: String) = InternalServerError(error)
+
+  def invalidInput(error: String) = BadRequest(error)
+
+  private lazy val APPLICATION_JSON = "application/json"
+}
