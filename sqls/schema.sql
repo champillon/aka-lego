691f4892ba349e95caadb507078a652b194df36d
passapong.t <passapong.t@gmail.com>
2017-08-17 14:37:34 +0700

    renamed thermals to fruiting

--- sqls/schema.sql
+++ sqls/schema.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS thermals;
+DROP TABLE IF EXISTS fruiting;
 
-CREATE TABLE thermals
+CREATE TABLE fruiting
 (
     sensor_id                   VARCHAR(64) NOT NULL,
     thermal                     INT NOT NULL,
     created                     TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
-ALTER TABLE thermals OWNER TO akalego;
+ALTER TABLE fruiting OWNER TO akalego;
