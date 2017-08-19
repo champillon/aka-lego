7608096d37b4d9de4fb38e10eb69789024e856f1
passapong.t <passapong.t@gmail.com>
2017-08-19 14:52:27 +0700

    added id to fruiting

--- sqls/schema.sql
+++ sqls/schema.sql
@@ -2,11 +2,13 @@ DROP TABLE IF EXISTS fruiting;
 
 CREATE TABLE fruiting
 (
+    id                          VARCHAR(64) NOT NULL,
     sensor_id                   VARCHAR(64) NOT NULL,
     thermal                     REAL,
     humidity                    REAL,
     co2                         REAL,
-    created                     TIMESTAMP WITH TIME ZONE NOT NULL
+    created                     TIMESTAMP WITH TIME ZONE NOT NULL,
+    CONSTRAINT  fruiting_pk PRIMARY KEY (id)
 );
 
 ALTER TABLE fruiting OWNER TO akalego;
