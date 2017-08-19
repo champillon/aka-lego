acd5d196b378c3e341a996ae2a5fb4a1a664987a
passapong.t <passapong.t@gmail.com>
2017-08-19 11:24:53 +0700

    added humidity, co2 and changed thermal to Double

--- app/forms/FruitingForm.scala
+++ app/forms/FruitingForm.scala
@@ -2,5 +2,8 @@ package forms
 
 import utilities.Json
 
-case class FruitingForm(sensorId: String, thermal: String)
+case class FruitingForm(sensorId: String
+                        , thermal: Double
+                        , humidity: Double
+                        , co2: Double)
   extends Json
