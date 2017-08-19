5002b190c9337870083b84c1aa0269f898a696c8
passapong.t <passapong.t@gmail.com>
2017-08-19 11:24:01 +0700

    added humidity, co2 and changed thermal to Double

--- app/models/Fruiting.scala
+++ app/models/Fruiting.scala
@@ -4,7 +4,9 @@ import entities.FruitingEntity
 
 case class Fruiting(id: String
                     , sensorId: String
-                    , thermal: Int)
+                    , thermal: Double
+                    , humidity: Double
+                    , co2: Double)
 
 object Fruiting {
 
@@ -13,5 +15,7 @@ object Fruiting {
       id = entity.id
       , sensorId = entity.sensorId
       , thermal = entity.thermal
+      , humidity = entity.humidity
+      , co2 = entity.co2
     )
 }
