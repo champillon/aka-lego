c1d14c0a796a6cf6e166bc3f99cdaa5f8d1760bd
passapong.t <passapong.t@gmail.com>
2017-09-02 10:14:05 +0700

    renamed errorHandling to handling and changed handling to protected

--- app/controllers/base/BasedController.scala
+++ app/controllers/base/BasedController.scala
@@ -14,14 +14,14 @@ abstract class BasedController(bcc: BasedControllerComponents)
       case _ if !request.hasBody => invalidInput(NO_BODY)
       case _ if request.body.asJson.isEmpty => invalidInput(NOT_JSON)
       case _ if as[Form](request).isEmpty => invalidInput(INVALID_JSON)
-      case _ => errorHandling(action)
+      case _ => handling(action)
     }
   }
 
   protected def as[Object: Manifest](request: Request[AnyContent]): Option[Object] =
     Json.toObject[Object](request.body.asJson.get.toString)
 
-  private def errorHandling(action: => Json) = {
+  protected def handling(action: => Json) = {
     try {
       success(action)
     } catch {
